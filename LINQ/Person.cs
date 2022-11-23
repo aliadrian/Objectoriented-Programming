f62836a68581e41e03468fa8007cad52d57c8acf
Ali Nasrat <ali.nasrat@brightstraining.com>
2022-11-23 21:37:29 +0100

    LINQ

---
+++ LINQ/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class Person : IEqualityComparer<Person>
+    {
+        public string Name { get; set; }
+        public DateTime NameDay { get; set; }
+
+        public Person(string name, DateTime nameDay)
+        {
+            Name = name;
+            NameDay = nameDay;
+        }
+
+        //public static Person ConvertStringToPerson(string s)
+        //{
+        //    string[] info = s.Split(';');
+        //    return new Person(info[0], DateTime.Parse(info[1]));
+        //}
+
+        //public override string ToString()
+        //{
+        //    if (Name.Length > 7)
+        //        return $"{Name}\t{NameDay.ToShortDateString()}";
+        //    else
+        //        return $"{Name}\t\t{NameDay.ToShortDateString()}";
+        //}
+
+        //public bool Equals(Person x, Person y)
+        //{
+        //    return x.Name == y.Name;
+        //}
+
+        //public int GetHashCode([DisallowNull] Person obj)
+        //{
+        //    return obj.Name.GetHashCode() + obj.NameDay.GetHashCode();
+        //}
+    }
+}
---
+++ LINQ/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace LINQ
+{
+    internal class Program
+    {
+        private const string Path = @"C:\Users\AliNasrat\source\repos\Objectoriented Programming\LINQ\names.csv";
+
+        private static List<Person> people;
+        private static void Main(string[] args)
+        {
+            var query = File.ReadAllLines(Path, System.Text.Encoding.Latin1)
+                .Distinct()
+                .Select(x => x.Split(';'))
+                .Select(x => new Person(x[0], DateTime.Parse(x[1])))
+                .ToList();
+
+            //ListAllNamesThatBeginsWithAn(query, "an");
+            ListNameWithNamnsdagSpecificDate(query, 12, 21);
+        }
+
+        private static void ListNameWithNamnsdagSpecificDate(List<Person> query2, int month, int day)
+        {
+            var q2 = query2
+                .Where(x => x.NameDay.Month == month && x.NameDay.Day == day)
+                .ToList();
+
+            foreach (var nameDay in q2)
+            {
+                Console.WriteLine(nameDay);
+            }
+        }
+
+        private static void ListAllNamesThatBeginsWithAn(List<Person> query1, string s)
+        {
+            var q = query1
+              .Where(x => x.Name.ToLower().StartsWith(s.ToLower()))
+              .Select(x => x.Name)
+              .ToList();
+
+            foreach (var personName in q)
+            {
+                Console.WriteLine(personName);
+            }
+        }
+    }
+}
